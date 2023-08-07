5f2d3b848edc8c7d091d3d17031222b4b79da617
Abdullah Naim <aanaim96@gmail.com>
2023-08-07 13:00:45 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
+
 app.use(express.static('public'));
 app.use(express.static('scripts'));
 app.use(express.static('style'))
@@ -9,12 +10,13 @@ app.use(express.static('style'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.post('/', function (req, res) {
-    res.send('success')
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 app.listen(port, () => {
